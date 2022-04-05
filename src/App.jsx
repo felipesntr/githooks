296d317b3c81eb3516c2ee75af0ff4560a99415d
Felipe Santos <rfelipe273@gmail.com>
2022-04-05 14:43:09 -0300

    feat: add repositories list

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,51 @@
+import { useState, useEffect } from "react";
+import "./app.css";
 function App() {
-  return <div className="App"></div>;
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        "https://api.github.com/users/diego3g/repos"
+      );
+      const data = await response.json();
+      setRepositories(data);
+    })();
+  }, []);
+
+  useEffect(() => {
+    let favorites = 0;
+    for (let i = 0; i < repositories.length; i++) {
+      if (repositories[i].favorite) favorites++;
+    }
+    window.document.title = `Você tem ${favorites} favoritos`;
+  }, [repositories]);
+
+  const toggleFavorite = (id) => {
+    const newRepositories = repositories.map((repository) => {
+      if (repository.id === id) {
+        return {
+          ...repository,
+          favorite: !repository.favorite,
+        };
+      }
+      return repository;
+    });
+    setRepositories(newRepositories);
+  };
+
+  return (
+    <>
+      <ul className="repo-list">
+        {repositories.map((repository) => (
+          <li key={repository.id} onClick={() => toggleFavorite(repository.id)}>
+            <a href={repository.svn_url}>{repository.name}</a>
+            {repository.favorite && <span> *</span>}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
